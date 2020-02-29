40fdd0cfda1af59e36069026267ee84bd76d70f9
David Wilkins <wilkidav@gmail.com>
2020-02-28 19:12:42 -0500

    refactored Playlist class

--- archive.py
+++ archive.py
@@ -14,48 +14,45 @@ DJESSE = 'https://www.youtube.com/playlist?list=OLAK5uy_nhzGQeVePsahNKbIK1U6qLRY
 
 class Playlist():
 
+    # For future compatibility with other video hosting sites.
     SOURCES = {
-        'youtube': 'https://www.youtube.com'
+        'youtube': 'https://www.youtube.com',
     }
 
-    def __init__(self, title, author, urls, source='youtube'):
-        self.title = title
-        self.author = author
-        self.urls = []
+    def __init__(self, url, source='youtube'):
+        self.url = url
         self.source = source
+        self.scrape_info()
         self.destination = f"{self.author} - {self.title}/"
-
-        for url in urls:
-            self.urls.append(self.SOURCES[source] + url)
 
     def __str__(self):
         return f"{self.author}: {self.title}"
 
+    def scrape_info(self):
+        r = requests.get(self.url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        self.title = soup.find(class_="pl-header-title").text.strip('\n').strip()
+        self.author = soup.find(class_="pl-header-details").find('a').text
+        playlist = soup.find(id='pl-video-list')
+        rows = playlist.find_all('tr')
+
+        urls = []
+        for row in rows:
+            # Combine scrape urls for tracks with BASE YouTube URL
+            url = row.find('a').get('href')
+            url = self.SOURCES[self.source] + url
+            urls.append(url)
+
+        self.urls = urls
+
     def download(self):
         for url in self.urls:
             yt = YouTube(url)
-            stream = yt.streams.filter(only_audio=True).first()
+            stream = yt.streams.get_audio_only()
             stream.download(self.destination)
-
-
-def make_playlist(playlist_url):
-    """Takes a YouTube playlist URL and returns a Playlist instance with Title, Author, and Individual track URLs."""
-
-    r = requests.get(playlist_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    title = soup.find(class_="pl-header-title").text.strip('\n').strip()
-    author = soup.find(class_="pl-header-details").find('a').text
-    playlist = soup.find(id='pl-video-list')
-    rows = playlist.find_all('tr')
-
-    urls = []
-    for row in rows:
-        urls.append(row.find('a').get('href'))
-
-    return Playlist(title, author, urls)
 
 
 if __name__ == "__main__":
     url = input("Enter Playlist URL: ")
-    p = make_playlist(url)
+    p = Playlist(url)
     p.download()
